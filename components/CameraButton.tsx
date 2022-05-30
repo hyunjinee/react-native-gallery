a5bc608f8cc3c75a7c2bb02ab602d2868332bb67
hyunjin <leehj0110@kakao.com>
2022-05-30 22:32:28 +0900

    feat: Modal 구현

--- components/CameraButton.tsx
+++ components/CameraButton.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import {View, Pressable, StyleSheet, Platform} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Pressable,
+  StyleSheet,
+  Platform,
+  ActionSheetIOS,
+} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import UploadModeModal from './UploadModeModal';
 
 const TABBAR_HEIGHT = 49;
 
 function CameraButton() {
+  const [modalVisible, setModalVisible] = useState(false);
   const insets = useSafeAreaInsets();
 
   const bottom = Platform.select({
@@ -13,16 +21,44 @@ function CameraButton() {
     ios: TABBAR_HEIGHT / 2 + insets.bottom - 4,
   });
 
+  const onPress = () => {
+    if (Platform.OS === 'android') {
+      setModalVisible(true);
+      return;
+    }
+
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['카메라로 촬영하기', '사진 선택하기', '취소'],
+        cancelButtonIndex: 2,
+      },
+      buttonIndex => {
+        if (buttonIndex === 0) {
+          console.log('camera');
+        } else if (buttonIndex === 1) {
+          console.log('picture');
+        }
+      },
+    );
+  };
+
   return (
-    <View style={[styles.wrapper, {bottom}]}>
-      <Pressable
-        android_ripple={{
-          color: '#ffffff',
-        }}
-        style={styles.circle}>
-        <Icon name="camera-alt" color="white" size={24} />
-      </Pressable>
-    </View>
+    <>
+      <View style={[styles.wrapper, {bottom}]}>
+        <Pressable
+          android_ripple={{
+            color: '#ffffff',
+          }}
+          style={styles.circle}
+          onPress={onPress}>
+          <Icon name="camera-alt" color="white" size={24} />
+        </Pressable>
+      </View>
+      <UploadModeModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+      />
+    </>
   );
 }
 
---
+++ components/UploadModeModal.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Modal, Pressable, Text, View, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+interface UploadModeModalProps {
+  visible: boolean;
+  onClose: () => void;
+}
+
+function UploadModeModal({visible, onClose}: UploadModeModalProps) {
+  return (
+    <Modal
+      visible={visible}
+      transparent={true}
+      animationType="fade"
+      onRequestClose={onClose}>
+      <Pressable style={styles.background} onPress={onClose}>
+        <View style={styles.whiteBox}>
+          <Pressable
+            style={styles.actionButton}
+            android_ripple={{color: '#eee'}}>
+            <Icon
+              name="camera-alt"
+              color="#757575"
+              style={styles.icon}
+              size={24}
+            />
+            <Text style={styles.text}>카메라로 촬영하기</Text>
+          </Pressable>
+          <Pressable
+            android_ripple={{color: '#eee'}}
+            style={styles.actionButton}>
+            <Icon name="photo" color="#757575" style={styles.icon} size={24} />
+            <Text style={styles.text}>사진 선택하기</Text>
+          </Pressable>
+        </View>
+      </Pressable>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  whiteBox: {
+    width: 300,
+    backgroundColor: 'white',
+    borderRadius: 4,
+    elevation: 2,
+  },
+  actionButton: {
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 8,
+  },
+  text: {
+    fontSize: 16,
+  },
+});
+
+export default UploadModeModal;
