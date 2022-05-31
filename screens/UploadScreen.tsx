ed89c9ac5b6d9a6063455a851210d278e073de59
hyunjin <leehj0110@kakao.com>
2022-05-31 19:38:28 +0900

    feat: animated picture by keyboard layout

--- screens/UploadScreen.tsx
+++ screens/UploadScreen.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {Image, StyleSheet, useWindowDimensions, View} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  useWindowDimensions,
+  View,
+  Animated,
+  Keyboard,
+} from 'react-native';
 import {useRoute} from '@react-navigation/native';
 
 import {UploadScreenRouteProp} from './RootStack';
@@ -7,22 +14,66 @@ import {UploadScreenRouteProp} from './RootStack';
 function UploadScreen() {
   const route = useRoute<UploadScreenRouteProp>();
   const {width} = useWindowDimensions();
+  const animation = useRef(new Animated.Value(width)).current;
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
   const {res} = route.params || {};
 
+  useEffect(() => {
+    const didShow = Keyboard.addListener('keyboardDidShow', () =>
+      setIsKeyboardOpen(true),
+    );
+
+    const didHide = Keyboard.addListener('keyboardDidHide', () =>
+      setIsKeyboardOpen(false),
+    );
+
+    return () => {
+      didShow.remove();
+      didHide.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: isKeyboardOpen ? 0 : width,
+      useNativeDriver: false,
+      duration: 150,
+      delay: 100,
+    }).start();
+  }, [isKeyboardOpen, width, animation]);
+
   return (
     <View style={styles.block}>
-      <Image
+      <Animated.Image
         source={{uri: res?.assets![0].uri}}
-        style={[styles.image, {height: width}]}
+        style={[styles.image, {height: animation}]}
+      />
+      <TextInput
+        style={styles.input}
+        multiline={true}
+        placeholder="이 사진에 대한 설명을 입력해주세요."
+        textAlignVertical="top"
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  block: {},
-  image: {},
+  block: {
+    flex: 1,
+  },
+  image: {
+    width: '100%',
+  },
+  input: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+    // backgroundColor: 'red',
+    flex: 1,
+    fontSize: 16,
+  },
 });
 
 export default UploadScreen;
